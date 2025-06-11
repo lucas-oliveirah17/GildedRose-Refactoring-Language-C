bbe135e9a6caa0da7da7fb809858d3372f78cf0f
lucas-oliveirah17 <lucas.oliveirah17@gmail.com>
2025-06-11 14:31:31 -0300

    TestBackstage_IncreasesQualityBy2_When10DaysOrLess

--- GildedRoseUnitTests.cc
+++ GildedRoseUnitTests.cc
@@ -86,6 +86,18 @@ TEST(TestGildedRoseGroup, TestSulfuras_NeverChanges)
     STRCMP_EQUAL("Sulfuras, Hand of Ragnaros", items[0].name);
 }
 
+TEST(TestGildedRoseGroup, TestBackstage_ncreasesQualityBy2_When10DaysOrLess)
+{
+    Item items[1];
+    init_item(items, "Backstage passes to a TAFKAL80ETC concert", 10, 20);
+    
+    update_quality(items, 1);
+
+    CHECK_EQUAL(9, items[0].sellIn);
+    CHECK_EQUAL(22, items[0].quality);
+    STRCMP_EQUAL("Backstage passes to a TAFKAL80ETC concert", items[0].name);
+}
+
 void example()
 {
     Item items[6];
